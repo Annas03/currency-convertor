7d0ad3eded880ff12805368e39ce271356b33302
Annas Baig <annas.baig@iomechs.com>
2025-11-15 10:05:19 +0500

    fix/be cors error

--- currency-convertor-server/src/main.ts
+++ currency-convertor-server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://currency-convertor-chi-ashy.vercel.app/',
+    origin: 'https://currency-convertor-chi-ashy.vercel.app',
     credentials: true,
   });
 
